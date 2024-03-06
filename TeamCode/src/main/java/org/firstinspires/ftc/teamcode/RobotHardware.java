39dea9cce592643d1ade53ee933acc568da7f049
Nathan Morris <nmorris@alberton.k12.mt.us>
2024-03-05 21:11:15 -0700

    Mr. Morris added RobotHardware.java to team code and updated it to match our hardware configuration. Work still needs to be done to implement wrist and gripper functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -37,7 +37,7 @@ import com.qualcomm.robotcore.util.Range;
  * Please read the explanations in that Sample about how to use this class definition.
  *
  * This file defines a Java Class that performs all the setup and configuration for a sample robot's hardware (motors and sensors).
- * It assumes three motors (left_drive, right_drive and arm) and two servos (left_hand and right_hand)
+ * It assumes six motors (left_front_drive, left_rear_drive, right_front_drive, right_rear_drive, arm_rotate, and arm_extend) and two servos (wrist and gripper)
  *
  * This one file/class can be used by ALL of your OpModes without having to cut & paste the code each time.
  *
@@ -51,23 +51,30 @@ import com.qualcomm.robotcore.util.Range;
  *
  */
 
+////Mr. Morris made changes to have 4 drive motors, arm rotation and extension, and wrist and gripper servos
+
 public class RobotHardware {
 
     /* Declare OpMode members. */
     private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
 
     // Define Motor and Servo objects  (Make them private so they can't be accessed externally)
-    private DcMotor leftDrive   = null;
-    private DcMotor rightDrive  = null;
-    private DcMotor armMotor = null;
-    private Servo   leftHand = null;
-    private Servo   rightHand = null;
+    private DcMotor leftFrontDrive   = null;
+    private DcMotor leftRearDrive   = null;
+    private DcMotor rightFrontDrive  = null;
+    private DcMotor rightRearDrive  = null;
+    private DcMotor armRotate = null;
+    private DcMotor armExtend = null;
+    private Servo   wrist = null;
+    private Servo   gripper = null;
 
     // Define Drive constants.  Make them public so they CAN be used by the calling OpMode
     public static final double MID_SERVO       =  0.5 ;
     public static final double HAND_SPEED      =  0.02 ;  // sets rate to move servo
     public static final double ARM_UP_POWER    =  0.45 ;
     public static final double ARM_DOWN_POWER  = -0.45 ;
+    public static final double ARM_EXTEND_POWER  = 0.10 ;
+    public static final double ARM_RETRACT_POWER  = -0.10 ;
 
     // Define a constructor that allows the OpMode to pass a reference to itself.
     public RobotHardware(LinearOpMode opmode) {
@@ -82,25 +89,34 @@ public class RobotHardware {
      */
     public void init()    {
         // Define and Initialize Motors (note: need to use reference to actual OpMode).
-        leftDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_drive");
-        armMotor   = myOpMode.hardwareMap.get(DcMotor.class, "arm");
+        leftFrontDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftRearDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_rear_drive");
+        rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightRearDrive  = myOpMode.hardwareMap.get(DcMotor.class, "right_rear_drive");
+        armRotate   = myOpMode.hardwareMap.get(DcMotor.class, "arm_rotate");
+        armExtend   = myOpMode.hardwareMap.get(DcMotor.class, "arm_extend");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        // Note: The settings here assume a 90 Deg drive on all four wheels. A Gear Reduction or direct drive may require direction flips
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
-        // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Since there are encoders connected, RUN_USING_ENCODER mode is enabled for greater accuracy
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armRotate.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Define and initialize ALL installed servos.
-        leftHand = myOpMode.hardwareMap.get(Servo.class, "left_hand");
-        rightHand = myOpMode.hardwareMap.get(Servo.class, "right_hand");
-        leftHand.setPosition(MID_SERVO);
-        rightHand.setPosition(MID_SERVO);
+        wrist = myOpMode.hardwareMap.get(Servo.class, "wrist");
+        gripper = myOpMode.hardwareMap.get(Servo.class, "gripper");
+        wrist.setPosition(MID_SERVO);
+        gripper.setPosition(MID_SERVO);
 
         myOpMode.telemetry.addData(">", "Hardware Initialized");
         myOpMode.telemetry.update();
@@ -127,29 +143,36 @@ public class RobotHardware {
             right /= max;
         }
 
-        // Use existing function to drive both wheels.
+        // Use existing function to drive wheels on both sides..
         setDrivePower(left, right);
     }
 
     /**
      * Pass the requested wheel motor powers to the appropriate hardware drive motors.
      *
+     * Mr. Morris: "Our robot is 4 wheel drive, but in tank drive configuration the two left wheels
+     * always travel at the same speed and the two right wheels travel at the same speed."
+     *
      * @param leftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
      * @param rightWheel    Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
      */
     public void setDrivePower(double leftWheel, double rightWheel) {
         // Output the values to the motor drives.
-        leftDrive.setPower(leftWheel);
-        rightDrive.setPower(rightWheel);
+        leftFrontDrive.setPower(leftWheel);
+        leftRearDrive.setPower(leftWheel);
+        rightFrontDrive.setPower(rightWheel);
+        rightRearDrive.setPower(rightWheel);
     }
 
     /**
      * Pass the requested arm power to the appropriate hardware drive motor
      *
-     * @param power driving power (-1.0 to 1.0)
+     * @param rotatePower driving power (-1.0 to 1.0)
+     * @param extendPower driving power (-1.0 to 1.0)
      */
-    public void setArmPower(double power) {
-        armMotor.setPower(power);
+    public void setArmPower(double rotatePower, double extendPower) {
+        armRotate.setPower(rotatePower);
+        armExtend.setPower(extendPower);
     }
 
     /**
@@ -157,9 +180,14 @@ public class RobotHardware {
      *
      * @param offset
      */
+
+    ////Mr. Morris: "TO DO: Update this to set positions for wrist and gripper, maybe as separate
+    ////                    functions"
+    /*
     public void setHandPositions(double offset) {
         offset = Range.clip(offset, -0.5, 0.5);
         leftHand.setPosition(MID_SERVO + offset);
         rightHand.setPosition(MID_SERVO - offset);
     }
+    */
 }
